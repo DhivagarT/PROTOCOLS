df6992741fd706d509a46f85e5cb775e4d2c9ed5
DHIVAGAR.T <dhivagardhanavel07@gmail.com>
2026-03-06 23:15:19 +0530

    Refactor slave module to apb_slave with updates

--- APB_PROTOCOL/SLAVE.sv
+++ APB_PROTOCOL/SLAVE.sv
@@ -1,47 +1,50 @@
-module slave
-  (input pclk,
-   input presetn,
-   
-   input pwrite,
-   input penable,
-   input psel,
-   input [7:0]pwdata,
-   input [7:0]paddr,
-   
-   output reg pready,
-   output reg [7:0]prdata,
-   output reg pslverr
-  );
-  
-//   parameter IDLE=0,SETUP=1,ACCESS=2;
-  
-//   reg [2:0]state;
-   reg [7:0]mem[0:255];
-  
-  
-always @(posedge pclk or negedge presetn) begin
-  if (!presetn) begin
-    pready<=0;
-    prdata<=0;
-    pslverr<=0;
-  end
-  else begin
-    // default outputs
-    pready<=0;
-    pslverr<=0;
+module apb_slave(
+    input pclk,
+    input preset_n,
 
-    // ACCESS phase
-    if (psel&&penable) begin
-      pready<=1;   // complete transfer
-      if(pwrite)begin
-        mem[paddr] <= pwdata;
-      end
-      else begin
-        prdata <= mem[paddr];
+  input psel,
+    input penb,
+  input [7:0]pwdata,
+  input [3:0]paddr,
+    input pwrite,
+
+    output reg pready,
+    output reg pslverr,
+  output reg [7:0]prdata
+);
+
+  reg [7:0]mem[0:15];
+ 
+  
+  always @(posedge pclk or negedge preset_n) begin
+    if(!preset_n) begin
+      pready <= 0;
+      pslverr <= 0;
+      prdata <= 0; 
+     
+    end
+    else begin
+      // Default values
+      pready <= 0;
+      pslverr <= 0;
+      
+      // ACCESS phase
+      if(psel && penb) begin
+        pready <= 1;   // ready in one cycle
+      
+        if(pwrite) begin
+          mem[paddr] <= pwdata;   // Write
+        end
+        else begin
+          if(paddr>4'd15 | pwdata>8'h31)  //address mismatch = slaverr=1 ,if addr and data mismatch
+            pslverr<=1;
+          else
+            pslverr<=0;
+          
+          prdata <= mem[paddr];   // Read
+        end
       end
     end
   end
-end
-
 
 endmodule
